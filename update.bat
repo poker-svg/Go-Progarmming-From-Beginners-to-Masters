85f86a179e24f729837ecfff2de155858b63e1f0
Xin <201220028@smail.nju.edu.cn>
2023-10-04 00:43:21 +0800

    2023-10-04: update note suggestion 14;finish read suggestion 16

--- update.bat
+++ update.bat
@@ -1,11 +1,4 @@
 @echo off
-
-echo updating the catalogue of this gitbook...
-book sm
-
-echo building the gitbook...
-gitbook build
-
 echo Which folder do you want to deploy to GitHub Pages?
 set /p var=
 echo Please input commit message:
